22a690b4d63ff4a51a3aee6a5b97b7c15945ba07
Sagar Latake <Sagarlatake@users.noreply.github.com>
2021-10-09 19:29:28 +0530

    Syntax

---
+++ 4 Syntax.py
@@ -0,0 +1,18 @@
+"""
+
+1.  Python syntax can be executed by
+    writing directly in the Command Line
+
+python terminal:
+
+Lets Go directly to terminal and see
+
+2. Indentation
+
+    Lets see the appropriate example
+
+3. Comment
+Comments start with a #, and
+Python will render the rest of the line as a comment
+
+"""
